511615a614aaaac6e9fe2750eb662538e4f6a764
Jae Choi <jaehyuk0325@gmail.com>
2021-01-07 23:23:05 -0700

    Initial Commit
    Initial Commit

---
+++ DataCrawling_API/.ipynb_checkpoints/Quandl_key-checkpoint.py
@@ -0,0 +1,3 @@
+# Quandl API
+# API key
+api_key='mxYDmP2yQw8sBHx4Se-X'
---
+++ DataCrawling_API/secrets.py
@@ -0,0 +1,9 @@
+# Twitter API World Sentiment Comparison
+# API key == consumer_key
+consumer_key = "pyhOc6XE5cD9sEsGTa1I4EEgO"
+# API secret key == consumer_secrets
+consumer_secret =  "K1IWOgw7vkispnQafItKjyoJIiLnOF38NrJ977o2Xzjd35OMEp"
+# Access token == access_key
+access_key = "1292162905098477569-emLuKK3yQxxBoSxoe84CDOvjqzNmcD"
+# Access token secret == access_secret
+access_secret = "eu9cSPuK1GKaida3KaiKc41EhiEa6U0klCe64l0Kf13DR"
